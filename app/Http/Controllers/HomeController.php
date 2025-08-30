185f0abf7ee4fef91a3a6a3ab9021a9226384194
Rochdi Karouali <rochdi.karouali1234@gmail.com>
2025-08-30 22:11:39 +0100

    udpate my home controller make sure my product shown on homepagge

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,20 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function pageView($routeName, $page = null)
     {
-        // Construct the view name based on the provided routeName and optional page parameter
-        $viewName = ($page) ? $routeName.'.'.$page : $routeName;
-        // Check if the constructed view exists
-        if (\View::exists($viewName)) {
-            // If the view exists, return the view
+        $viewName = ($page) ? $routeName . '.' . $page : $routeName;
+
+        if (view()->exists($viewName)) {
             return view($viewName);
         } else {
-            // If the view doesn't exist, return a 404 error
             abort(404);
         }
     }
