bcaa57a688945eb05d75488a285780ae5bd9d82f
Rochdi Karouali <rochdi.karouali1234@gmail.com>
2025-09-15 21:09:34 +0100

    product tags views blade add

--- resources/views/backoffice/product_tags/_form.blade.php
+++ resources/views/backoffice/product_tags/_form.blade.php
@@ -1,23 +1,16 @@
 @csrf
 
 <div class="row g-3">
-    <div class="col-md-6">
-        <label class="form-label">Nom du tag *</label>
-        <input type="text" name="name"
-               class="form-control @error('name') is-invalid @enderror"
-               value="{{ old('name', $tag->name ?? '') }}" required>
+    <div class="col-md-12">
+        <label class="form-label">Tags à créer *</label>
+        <textarea name="tags"
+                  rows="3"
+                  class="form-control @error('tags') is-invalid @enderror"
+                  placeholder="Ex: Urgence, Chirurgie, Diagnostic"
+                  required>{{ old('tags') }}</textarea>
+        <small class="text-muted">Séparez les tags par une virgule. Exemple : <em>Urgence, Chirurgie, Diagnostic</em></small>
         <div class="invalid-feedback">
-            @error('name') {{ $message }} @else Ce champ est requis. @enderror
-        </div>
-    </div>
-
-    <div class="col-md-6">
-        <label class="form-label">Slug (optionnel)</label>
-        <input type="text" name="slug"
-               class="form-control @error('slug') is-invalid @enderror"
-               value="{{ old('slug', $tag->slug ?? '') }}">
-        <div class="invalid-feedback">
-            @error('slug') {{ $message }} @else Slug invalide. @enderror
+            @error('tags') {{ $message }} @else Veuillez saisir au moins un tag. @enderror
         </div>
     </div>
 </div>
