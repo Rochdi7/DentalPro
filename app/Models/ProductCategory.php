61df516076d3201b432de77d20591ae0d4f83697
Rochdi Karouali <rochdi.karouali1234@gmail.com>
2025-09-20 19:46:56 +0100

    udpate my models

--- app/Models/ProductCategory.php
+++ app/Models/ProductCategory.php
@@ -33,19 +33,55 @@ class ProductCategory extends Model
         return $query->where('is_active', true);
     }
 
-
+    /**
+     * Get the parent category.
+     */
     public function parent()
     {
         return $this->belongsTo(ProductCategory::class, 'parent_id');
     }
+
+    /**
+     * Get the subcategories (direct children).
+     */
     public function children()
     {
         return $this->hasMany(ProductCategory::class, 'parent_id');
     }
+
+    /**
+     * Get all descendants (children, grandchildren, etc.).
+     * This is a recursive relationship.
+     */
+    public function descendants()
+    {
+        return $this->children()->with('descendants');
+    }
+
+    /**
+     * Get the products for this specific category.
+     */
     public function products()
     {
         return $this->hasMany(Product::class, 'product_category_id');
     }
 
-    
+    /**
+     * Get all products for this category and its subcategories (all descendants).
+     *
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function allProducts()
+    {
+        // Get the IDs of all descendant categories recursively
+        $descendantIds = $this->descendants()->get()->flatMap(function ($category) {
+            return array_merge([$category->id], $category->descendants->pluck('id')->all());
+        })->all();
+        
+        // Combine the current category's ID with all descendant IDs
+        $categoryIds = array_merge([$this->id], $descendantIds);
+        
+        // Return a query builder for all products in these categories
+        return Product::whereIn('product_category_id', $categoryIds);
+    }
 }
